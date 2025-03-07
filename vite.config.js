61ea824c20b0431a936bfc88a6d089a85c9e14b3
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-03-07 11:30:57 -0500

    Move index.html to root

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { babel } from '@rollup/plugin-babel';
 export default defineConfig(({ mode }) => {
   if (mode === 'test') {
     return {
+      build: { rollupOptions: { input: { main: 'index.html' } } },
       plugins: [
         ember(),
         babel({
